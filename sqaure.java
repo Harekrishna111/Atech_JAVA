7398fdf1b3296c464be06c7a19e6acef5d24ab40
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-10-23 04:49:39 +0530

    sqaure of no:

---
+++ sqaure.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;public class sqaure {
+    public static void main(String[] args) {
+        int number = 7;
+        int square = number * number;
+        System.out.println("The square of " + number + " is: " + square);
+
+        double decimalNumber = 3.5;
+        double decimalSquare = decimalNumber * decimalNumber;
+        System.out.println("The square of " + decimalNumber + " is: " + decimalSquare);
+    }
+}
