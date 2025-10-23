8b297ac22be157f8df1534094cb8cb7a122cfcb1
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-10-23 05:43:36 +0530

    widening & narrowing

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+
+public class main {
+    public static void main(String[] args) {
+//        int num = 98;
+//        System.out.println(num);
+// widening
+//        float n = num;
+//        System.out.println(n);
+
+//float num = 3.0F;
+//double n = num;
+//System.out.println(n);
+
+//Narrowing
+//        float n =8.33333F;
+//        int num =  (int) n ;
+//        System.out.println(num);
+
+        double n =8.33338F;
+        int num =  (int) n ;
+        System.out.println(num);
+
+    }
+}
