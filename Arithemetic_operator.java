69aacf67d502cd4a29349642b1ddffd44a2a37e0
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-10-25 04:54:56 +0530

    Arithmeric operator

---
+++ Arithemetic_operator.java
@@ -0,0 +1,11 @@
+public class Arithemetic_operator {
+    public static void main(String [] args) {
+        int a = 4;
+        int b = 4;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
