8bdfc62a9f676a329c485faa84d3f66be73a67e5
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-10-25 05:12:06 +0530

    Assignment operator

---
+++ Assignment_operator.java
@@ -0,0 +1,22 @@
+public class Assignment_operator {
+    public static void main(String [] args){
+        int a = 12;
+        int b = 4;
+        int c = 20;
+        int d = 9;
+        int e = 1;
+        int f = 5;
+        a += 2;
+        b -= 3;
+        c *= 10;
+        d /= 1 ;
+        e %= 3;
+        f = 10;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
