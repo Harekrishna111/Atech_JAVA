9d5b3cd7fac327b79ce6a8a4253eb814a011ba4b
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-10-26 18:04:59 +0530

    bitwise_operator learn bitwise OR

--- bitwise_operator.java
+++ bitwise_operator.java
@@ -2,8 +2,11 @@ public class bitwise_operator {
     public static void  main(String[] args){
 //        int a= 5;//0101
 //        int b=6;//0110
-        int a = 8;
-        int b = 9;
+//        int a = 8;
+//        int b = 9;
+        int a= 12;
+        int b = 5;
         System.out.println(a&b);
+        System.out.println(a|b);
     }
 }
