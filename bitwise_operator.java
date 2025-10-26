ee3eb99564118d66452074d5b40a81c1a066377f
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-10-26 17:36:01 +0530

    bitwise_operator

---
+++ bitwise_operator.java
@@ -0,0 +1,9 @@
+public class bitwise_operator {
+    public static void  main(String[] args){
+//        int a= 5;//0101
+//        int b=6;//0110
+        int a = 8;
+        int b = 9;
+        System.out.println(a&b);
+    }
+}
