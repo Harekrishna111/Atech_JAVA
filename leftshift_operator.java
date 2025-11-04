e3a07c73e6dbb07c22111b5c17154b1862676393
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-11-04 22:34:05 +0530

    leftshift_operator

---
+++ leftshift_operator.java
@@ -0,0 +1,8 @@
+public class leftshift_operator {
+    public static void main(String[] args){
+        int a=12;
+//        int b =a<<1;
+        int b = a<<2;
+        System.out.println(b);
+    }
+}
