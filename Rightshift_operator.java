556ba6e2abb36ae67303f85009459abfd3a33162
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-11-07 05:13:20 +0530

    rightshift_operator

--- Rightshift_operator.java
+++ Rightshift_operator.java
@@ -1,8 +1,8 @@
 public class Rightshift_operator {
     public static void main(String[] args){
-        int a= 12;
+        int a= 100;
 //        int b =a>>1;
-        int b =a>>3;
+        int b =a>>2;
         System.out.println(b);
     }
 }
