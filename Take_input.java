133fe30ce2ff1a14a31b83924e75b119d6d45b3b
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-11-07 05:33:24 +0530

    taking input

--- Take_input.java
+++ Take_input.java
@@ -1,9 +1,34 @@
+//import java.util.Scanner;
+//public class Take_input {
+//    public static  void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a int value");
+//        int a = sc.nextInt();
+//        System.out.println(a);
+//    }
+//}
+
+
 import java.util.Scanner;
+
 public class Take_input {
-    public static  void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a int value");
+
+        System.out.print("Enter an Integer value: ");
         int a = sc.nextInt();
-        System.out.println(a);
+
+        System.out.print("Enter a Float value: ");
+        float b = sc.nextFloat();
+
+        System.out.print("Enter a String: ");
+        String c = sc.next();
+
+        System.out.println("Int: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("String: " + c);
+
+        sc.close();
     }
 }
+
