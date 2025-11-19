1f565cb830fec8bc163d98280a1a7e07e1de5e8c
Harekrishna111 <harekrishnapramanik535@gmail.com>
2025-11-20 05:16:33 +0530

    if else

---
+++ control_flow_statement.java
@@ -0,0 +1,12 @@
+public class control_flow_statement {
+    public static void main(String[] args){
+        int i = 3;
+        if(i<89){
+            System.out.println("small");
+
+        }else{
+            System.out.println("big");
+        }
+    }
+}
+
